154bb2e35822f6ecad13e422a0764dd0b8f85e4d
Joshua Lucas <email": null>
2017-02-13 17:52:14 -0600

    create signup form and user controller routes

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,7 @@ class Question < ApplicationRecord
   belongs_to :votable, polymorphic: true
   has_many :votes, as: :votable
 
+  validates :title, presence: true
+  validates :text, presence: true
+
 end
