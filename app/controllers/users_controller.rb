b8566b989821c5ee6c322f30013ea77f82aa1479
Joshua Lucas <email": null>
2017-02-13 17:55:16 -0600

    generate Questions controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  # befor_action :require_login
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    puts params
+    if @user.save
+      questions_path
+    else
+      @errors = @user.errors.full_messages
+      redirect_to(:back)
+    end
+  end
+
+
+  private
+
+  def user_params
+    params[:user].permit(:username, :password, :email)
+  end
+end
+
+
